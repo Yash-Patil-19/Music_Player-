a1a5e9e8f2c092843926e10fcca36b5d558909df
Yash Patil <yp.atil2040@gmail.com>
2025-01-30 18:37:09 +0530

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,38 @@
+let allMusic = [
+{
+    name: "Random & first - crazy",
+    artist:"Random first",
+    img:"music-1",
+    src:"music-1"
+},
+{
+    name:"Indon anywhere - Ikson",
+    artist:"Indon anywhere",
+    img:"music-2",
+    src:"music-2"
+},
+{
+    name:"Harley Gore - Home",
+    artist:"Harley Gore",
+    img:"music-3",
+    src:"music-3"
+},
+{
+    name:"James Jal - Rock",
+    artist:"James Jal",
+    img:"music-4",
+    src:"music-4"
+},
+{
+    name:"Library Cord - Gorzilla",
+    artist:"Library Cord",
+    img:"music-5",
+    src:"music-5"
+},
+{
+    name:"Frau Ghet - Es tut mir",
+    artist:"Frau Ghet",
+    img:"music-6",
+    src:"music-6"
+},
+]
